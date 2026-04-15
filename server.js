87f97af2b200a9bbc876c666f2c32b1c62d961ac
Xolani <mac@Xolanis-MacBook.local>
2026-04-15 22:43:04 +0200

    Fix Express routing

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const port = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const distPath = path.join(__dirname, "dist");
 
 app.use(express.static(distPath));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
